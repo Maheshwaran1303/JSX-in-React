5e64a86410e4be83fd8feba1937f216483314fc0
maheshwaran1303 <maheshwaran130324@gmail.com>
2025-08-28 02:35:24 +0530

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
-import JSXStyle from "./components/JSXStyle";
+// import JSXStyle from "./components/JSXStyle";
 // import JSXAttribute from "./components/JSXAttribute";
 // import JSXEmbedding from "./components/JSXEmbedding";
 
 function App() {
   return (
-    <>
+    <div>
+      <h1>Welcome to Learning JSX</h1>
+      <p>Uncomment the JSX components to see the magic</p>
       {/* <JSXEmbedding />, */}
       {/* <JSXAttribute /> */}
-      <JSXStyle />
-    </>
+      {/* <JSXStyle /> */}
+    </div>
   );
 }
 
